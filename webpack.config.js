f53df6d20e27e782387d23c04630a25e2f5105cc
Sushil Ganvir <38817891+sushilganvir@users.noreply.github.com>
2024-12-03 20:19:15 +0530

    initial

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: "./app.js",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
